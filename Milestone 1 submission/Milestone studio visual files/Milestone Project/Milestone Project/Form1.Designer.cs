0191843b63e443a8b5e8b10f3374331274d2b065
Naralla <104328391+Naralla@users.noreply.github.com>
2022-05-01 16:26:06 -0700

    Add files via upload

---
+++ Milestone 1 submission/Milestone studio visual files/Milestone Project/Milestone Project/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Milestone_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.stockRemainingBox = new System.Windows.Forms.Label();
+            this.orderMoreButton = new System.Windows.Forms.Button();
+            this.completeOrderButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(246, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Order Request Form";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Are you a member?\r\nEnter Membership Number\r\n";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(229, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Enter Product ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Enter Quantity ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(229, 184);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(155, 29);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(229, 246);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(155, 29);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(509, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(221, 22);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Total Quantity Remaining";
+            // 
+            // stockRemainingBox
+            // 
+            this.stockRemainingBox.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.stockRemainingBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.stockRemainingBox.Location = new System.Drawing.Point(513, 164);
+            this.stockRemainingBox.Name = "stockRemainingBox";
+            this.stockRemainingBox.Size = new System.Drawing.Size(217, 49);
+            this.stockRemainingBox.TabIndex = 8;
+            this.stockRemainingBox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // orderMoreButton
+            // 
+            this.orderMoreButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.orderMoreButton.Location = new System.Drawing.Point(562, 228);
+            this.orderMoreButton.Name = "orderMoreButton";
+            this.orderMoreButton.Size = new System.Drawing.Size(121, 47);
+            this.orderMoreButton.TabIndex = 9;
+            this.orderMoreButton.Text = "Order More";
+            this.orderMoreButton.UseVisualStyleBackColor = true;
+            // 
+            // completeOrderButton
+            // 
+            this.completeOrderButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.completeOrderButton.Location = new System.Drawing.Point(88, 311);
+            this.completeOrderButton.Name = "completeOrderButton";
+            this.completeOrderButton.Size = new System.Drawing.Size(169, 50);
+            this.completeOrderButton.TabIndex = 10;
+            this.completeOrderButton.Text = "Complete Order";
+            this.completeOrderButton.UseVisualStyleBackColor = true;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeButton.Location = new System.Drawing.Point(539, 390);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(131, 36);
+            this.closeButton.TabIndex = 11;
+            this.closeButton.Text = "Cancel Order";
+            this.closeButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 449);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.completeOrderButton);
+            this.Controls.Add(this.orderMoreButton);
+            this.Controls.Add(this.stockRemainingBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "Order Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label stockRemainingBox;
+        private System.Windows.Forms.Button orderMoreButton;
+        private System.Windows.Forms.Button completeOrderButton;
+        private System.Windows.Forms.Button closeButton;
+    }
+}
+
---
+++ Milestone 1 submission/Milestone studio visual files/Milestone Project/Milestone Project/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Dalton Danz
+//CST-150
+//April 24, 2022
+//This is my own work
+
+namespace Milestone_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
